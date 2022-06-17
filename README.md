146ff0b9c9432ab2ea7bad784bb30637baa41653
Ionut <moldovan.ionut.89@gmail.com>
2022-06-17 14:20:37 +0300

    sql files output

--- README.md
+++ README.md
@@ -1,21 +1,51 @@
 # FILE PARSER
 
-## Setup and running the bot
+## Setup and running
  1. `rustc main.rs`
- 2. Create a `config.ini` file - look at `config.ini.example` for more info
+ 2. Create a `config.ini` file - look at `config.ini.example` for more info.
+ 3. Create a `input` folder and drop in some files in there.
+ 4. Run the executable.
+ 5. Check the `errors.txt` file or the `output` folder.
 
 ## INI Configuration
-Consists of two sections:
-1. `ROW_CONFIG`:
+Consists of 4 sections:
+1. `CONFIG`:
+    - `INPUT_FOLDER` - folder where the files are
+    - `ROW_TYPES` - types of row to expect, comma separated
+    ```
+    ROW_TYPES=A,B,C 
+    ```
+2. `ROW_CONFIG`:
     - Each row type must be configured with the fields it contains; Fields are separated by a `+`. Supported fields types are `string`, `number` and `date`. The `date` field requires a format string that can be passed using `|` like ```date|format (date|%m-%d-%Y)```. Date format options can be found [here](https://docs.rs/chrono/0.3.1/chrono/format/strftime/index.html)
     - Field can be marked as optional with a `*`, a field marked with `*` will not throw an error when the value is empty. 
-
     ```
     A=string*+string+number+date|%m/%d/%Y
     ```
-2. `CONFIG`:
-    - `INPUT_FOLDER` - folder where the files are
-    - `IGNORE_EMPTY_LINES = 1` - ignore empty lines
 
+3. `COLUMN_NAMES`:
+    - Name of the sql table columns to be used when building the sql query. 
+     ```
+    A=Address+Phone
+    ```
+    
+4. `COLUMN_LINKS`:
+    - Add special columns, their value will be taken from other rows. Composed of the name of the column, followed by a ``->`` followed by the row type from where the value should be taken, followed by ``:``, followed by the number of column from where the value is taken.
+     ```
+    A=ColumnName->B:1+AnotherColumn->C:0
+    ```
+    
+5. `TABLE_NAMES`:
+    - Specify the table name to be used in queries for a specific type of row.
+     ```
+    A=MyTable
+    B=MyOtherTable
+    ```
 ## Output
+### Errors:
 All errors are outputed to `errors.txt`.
+### SQL FILES:
+Are outputed to the `output` folder, a `.sql` file for each row type.
+
+## Notes
+- No sql files are outputed if errors are detected.
+- `H` has hardocoded two fields `hdrFileNamem` (stores the file name),`hdrFileQuarter` (stores the last 4 chars of the file name, excluding the extension).
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ extern crate ini;
 use chrono::NaiveDate;
 use colored::Colorize;
 use ini::{Ini, Properties};
+use std::collections::HashMap;
 use std::fs::OpenOptions;
 use std::fs::{self, DirEntry, File};
 use std::io::prelude::*;
@@ -23,7 +24,14 @@ fn main() {
 
     let ini: Ini = Ini::load_from_file(ini_path).unwrap();
     let config: &Properties = ini.section(Some("CONFIG")).unwrap();
-    let row_types: &Properties = ini.section(Some("ROW_CONFIG")).unwrap();
+    let cfg_row_types: &Properties = ini.section(Some("ROW_CONFIG")).unwrap();
+    let cfg_column_names: &Properties = ini.section(Some("COLUMN_NAMES")).unwrap();
+    let cfg_table_names: &Properties = ini.section(Some("TABLE_NAMES")).unwrap();
+    let cfg_column_links: &Properties = ini.section(Some("COLUMN_LINKS")).unwrap();
+
+    if !validate_ini(cfg_column_names, cfg_row_types) {
+        process::exit(1);
+    }
 
     let config_input_path = config.get("INPUT_FOLDER").unwrap().to_string();
     let mut input_path: String = "./input".to_string();
@@ -63,10 +71,10 @@ fn main() {
         }
     }
 
-    println!("Processing {} files", &files.len());
+    println!("Checking {} files", &files.len());
 
     for (i, file) in files.iter().enumerate() {
-        let file_errors = process_file(&file, &row_types, &config);
+        let file_errors = check_file(&file, &cfg_row_types, &config);
         number_of_issues += file_errors.len();
         if file_errors.len() > 0 {
             errors.push(format!("FILE: {}", file.file_name().to_string_lossy()));
@@ -76,19 +84,269 @@ fn main() {
         }
         print!("\x1B[2J\x1B[1;1H");
         println!(
-            "Processed file {} of {}",
+            "Checked file {} of {}",
             (i + 1).to_string().green(),
             files.len().to_string().green()
         );
+    }
+    match fs::remove_file("errors.txt") {
+        Err(_e) => (),
+        _ => (),
     }
 
     if errors.len() > 0 {
         println!("Found {} issues", number_of_issues.to_string().red());
         write_errors(errors);
         println!("All issues can be found in {}", "errors.txt".green());
+        process::exit(1);
     } else {
         println!("{}", "Found No Issues".green());
+        let mut sqls: Vec<(String, String)> = Vec::<(String, String)>::new();
+        for (i, file) in files.iter().enumerate() {
+            let mut result: Vec<(String, String)> = process_file(
+                &file,
+                cfg_table_names,
+                cfg_column_names,
+                cfg_row_types,
+                cfg_column_links,
+            );
+            sqls.append(&mut result);
+            println!(
+                "Processed file {} of {}",
+                (i + 1).to_string().green(),
+                files.len().to_string().green()
+            );
+        }
+
+        match fs::remove_dir_all("./output") {
+            Err(_e) => (),
+            _ => (),
+        }
+
+        match fs::create_dir_all("./output") {
+            Err(_e) => println!("Error Creating Output Dir. Please create it manually"),
+            _ => (),
+        }
+
+        let mut sql_files: Vec<(String, File)> = Vec::<(String, File)>::new();
+        for (_, val) in config
+            .get("ROW_TYPES")
+            .unwrap()
+            .to_string()
+            .split(",")
+            .collect::<Vec<&str>>()
+            .iter()
+            .enumerate()
+        {
+            sql_files.push((
+                val.to_string(),
+                OpenOptions::new()
+                    .create(true)
+                    .truncate(false)
+                    .write(true)
+                    .open(["./output/", &val.to_string(), ".sql"].join(""))
+                    .unwrap(),
+            ));
+        }
+
+        for (_, value) in sqls.iter().enumerate() {
+            for (_, file_value) in sql_files.iter().enumerate() {
+                if value.0.to_string() == file_value.0.to_string() {
+                    match write!(&file_value.1, "{}", value.1) {
+                        Err(e) => println!("{:?}", e),
+                        _ => (),
+                    }
+
+                    match writeln!(&file_value.1, "\n") {
+                        Err(e) => println!("{:?}", e),
+                        _ => (),
+                    }
+                }
+            }
+        }
+
+        println!("Done");
     }
+}
+
+fn validate_ini(cfg_column_names: &Properties, cfg_row_types: &Properties) -> bool {
+    let mut valid: bool = true;
+    for (key, _value) in cfg_row_types.iter() {
+        if !valid {
+            continue;
+        }
+        if cfg_column_names
+            .get(key.to_string())
+            .unwrap()
+            .split("+")
+            .collect::<Vec<&str>>()
+            .len()
+            != cfg_row_types
+                .get(key.to_string())
+                .unwrap()
+                .split("+")
+                .collect::<Vec<&str>>()
+                .len()
+        {
+            valid = false;
+            println!(
+                "Invalid INI Config, please check the Columns Names and Column Types section of {:?}",
+                &key
+            );
+        }
+    }
+
+    return valid;
+}
+
+fn process_file(
+    file: &DirEntry,
+    cfg_table_names_conf: &Properties,
+    cfg_column_names: &Properties,
+    cfg_row_types: &Properties,
+    cfg_columns_links: &Properties,
+) -> Vec<(String, String)> {
+    let mut result: Vec<(String, String)> = Vec::<(String, String)>::new();
+    if let Ok(lines) = read_lines(file.path()) {
+        let mut previous_value = HashMap::new();
+
+        for (_i, line) in lines.enumerate() {
+            if let Ok(ip) = line {
+                let row_values: Vec<String> = ip.split("\t").map(|s| s.to_string()).collect();
+                let first_letter = row_values[0].chars().nth(0).unwrap();
+                let column_names = cfg_column_names
+                    .get(&first_letter.to_string())
+                    .unwrap()
+                    .split("+")
+                    .collect::<Vec<&str>>();
+
+                previous_value.insert(first_letter, row_values.clone());
+
+                let mut column_values: Vec<(String, String)> = Vec::<(String, String)>::new();
+
+                for (key, _value) in column_names.iter().enumerate() {
+                    column_values
+                        .push((column_names[key].to_string(), row_values[key].to_string()));
+                }
+
+                let table_name = cfg_table_names_conf.get(&first_letter.to_string()).unwrap();
+
+                let mut sql_statement: String = ["INSERT INTO ", table_name, " ( "].join("");
+
+                for (_key, value) in column_values.iter().enumerate() {
+                    sql_statement += &["\"", &(value.0.to_string()), "\"", ","].join("");
+                }
+
+                if first_letter == 'H' {
+                    sql_statement +=
+                        &[",\"", "hdrFileName", "\",", "\"hdrFileQuarter", "\","].join("");
+                }
+
+                let mut column_links = Vec::<&str>::new();
+
+                if cfg_columns_links.get(&first_letter.to_string()).is_some() {
+                    column_links = cfg_columns_links
+                        .get(&first_letter.to_string())
+                        .unwrap()
+                        .split("+")
+                        .collect::<Vec<&str>>();
+                }
+
+                if column_links.iter().len() > 0 {
+                    for (_key, value) in column_links.iter().enumerate() {
+                        sql_statement +=
+                            &[",\"", value.split("->").nth(0).unwrap(), "\","].join("");
+                    }
+                }
+
+                let row_types = cfg_row_types
+                    .get(&first_letter.to_string())
+                    .unwrap()
+                    .split("+")
+                    .collect::<Vec<&str>>();
+
+                sql_statement += ") VALUES (";
+                for (key, value) in column_values.iter().enumerate() {
+                    if !row_types[key].contains("number") {
+                        let mut string_value = value.1.to_string().replace("'", "\\'").clone();
+                        if &string_value.chars().last() == &Some('"') {
+                            string_value.pop();
+                        }
+
+                        if &string_value.chars().nth(0) == &Some('"') {
+                            string_value.remove(0);
+                        }
+
+                        sql_statement += &["'", &string_value, "'"].join("");
+                    } else {
+                        sql_statement += &value.1.to_string();
+                    }
+
+                    sql_statement += ",";
+                }
+
+                if first_letter == 'H' {
+                    let file_name: String = file.file_name().to_string_lossy().to_string();
+                    let file_name_no_ext = file_name.split(".").enumerate().nth(0).unwrap();
+
+                    sql_statement += &[
+                        ", '",
+                        &file_name,
+                        "', '",
+                        &file_name_no_ext.1[file_name_no_ext.1.len() - 4..],
+                        "',",
+                    ]
+                    .join("");
+                }
+
+                if column_links.iter().len() > 0 {
+                    for (_key, value) in column_links.iter().enumerate() {
+                        if value.len() > 0 {
+                            sql_statement += &[
+                                ",'",
+                                &previous_value
+                                    .get(
+                                        &value
+                                            .split("->")
+                                            .nth(1)
+                                            .unwrap()
+                                            .split(":")
+                                            .nth(0)
+                                            .unwrap()
+                                            .chars()
+                                            .nth(0)
+                                            .unwrap(),
+                                    )
+                                    .unwrap()
+                                    .iter()
+                                    .nth(
+                                        value
+                                            .split("->")
+                                            .nth(1)
+                                            .unwrap()
+                                            .split(":")
+                                            .nth(1)
+                                            .unwrap()
+                                            .parse::<usize>()
+                                            .unwrap(),
+                                    )
+                                    .unwrap(),
+                                "',",
+                            ]
+                            .join("");
+                        }
+                    }
+                }
+
+                sql_statement += ")";
+
+                sql_statement = sql_statement.replace(",)", ")").replace(",,", ",");
+
+                result.push((first_letter.to_string(), sql_statement));
+            }
+        }
+    }
+    return result;
 }
 
 fn validate_file_type(file_type: &str) -> bool {
@@ -98,7 +356,7 @@ fn validate_file_type(file_type: &str) -> bool {
     }
 }
 
-fn process_file(file: &DirEntry, row_types: &Properties, config: &Properties) -> Vec<String> {
+fn check_file(file: &DirEntry, row_types: &Properties, config: &Properties) -> Vec<String> {
     let mut file_errors: Vec<String> = Vec::new();
     if let Ok(lines) = read_lines(file.path()) {
         for (i, line) in lines.enumerate() {
@@ -110,9 +368,6 @@ fn process_file(file: &DirEntry, row_types: &Properties, config: &Properties) ->
                 }
 
                 if first_letter.is_whitespace() {
-                    if config.get("IGNORE_EMPTY_LINES").unwrap() == "1" {
-                        continue;
-                    }
                     file_errors.push(format!("#{}, Empty Line: {}", i + 1, first_letter));
                     continue;
                 }
@@ -132,7 +387,7 @@ fn process_file(file: &DirEntry, row_types: &Properties, config: &Properties) ->
                     if key == &first_letter.to_string() {
                         let fields = value.split("+").collect::<Vec<&str>>();
 
-                        if row_values.len() < fields.len() + 1 {
+                        if row_values.len() < fields.len() {
                             file_errors.push(format!(
                                 "#{}, Wrong number of fields, Line Type => {}",
                                 i + 1,
@@ -144,46 +399,49 @@ fn process_file(file: &DirEntry, row_types: &Properties, config: &Properties) ->
                         for (key, value) in fields.iter().enumerate() {
                             if value.contains("number") {
                                 if !value.contains("*") {
-                                    if row_values[key + 1].to_string().len() == 0 {
+                                    if row_values[key].to_string().len() == 0 {
                                         file_errors.push(format!(
                                             "#{}, Invalid Number at column #{}",
                                             i + 1,
-                                            key + 1
+                                            key
                                         ));
                                     }
-                                } else if !is_string_numeric(row_values[key + 1].to_string()) {
+                                } else if !is_string_numeric(row_values[key].to_string()) {
                                     file_errors.push(format!(
                                         "#{}, Invalid Number at column #{}",
                                         i + 1,
-                                        key + 1
+                                        key
                                     ));
                                 }
                             }
 
                             if value.contains("string")
                                 && !value.contains("*")
-                                && row_values[key + 1].len() == 0
+                                && row_values[key].len() == 0
                             {
                                 file_errors.push(format!(
                                     "#{}, Invalid String at column #{}",
                                     i + 1,
-                                    key + 1
+                                    key
                                 ));
                             }
-                            if value.contains("date") && !value.contains("*") {
-                                let format = value.split("|").collect::<Vec<&str>>()[1];
 
-                                match NaiveDate::parse_from_str(row_values[key + 1], format) {
-                                    Ok(_) => continue,
-                                    Err(_) => {
-                                        println!("{}:{}", row_values[key + 1], format);
-                                        file_errors.push(format!(
-                                            "#{}, Invalid Date at column #{}",
-                                            i + 1,
-                                            key + 1
-                                        ));
-                                    }
-                                };
+                            if value.contains("date") {
+                                if !value.contains("*")
+                                    || value.contains("*") && row_values[key].len() != 0
+                                {
+                                    let format = value.split("|").collect::<Vec<&str>>()[1];
+                                    match NaiveDate::parse_from_str(row_values[key], format) {
+                                        Ok(_) => continue,
+                                        Err(_) => {
+                                            file_errors.push(format!(
+                                                "#{}, Invalid Date at column #{}",
+                                                i + 1,
+                                                key
+                                            ));
+                                        }
+                                    };
+                                }
                             }
                         }
                     }
